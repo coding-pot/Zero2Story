77068f4e7ce83505213ee9b1a5c3bd7fd41a1993
Sangjoon Han (Coding-pot) <jphan32@me.com>
2023-09-17 09:34:04 +0900

    fix: typo

--- interfaces/ui.py
+++ interfaces/ui.py
@@ -28,11 +28,12 @@ def get_random_name(cur_char_name, char_name1, char_name2, char_name3):
 
 def gen_character_image(
   gallery_images, 
-  time, place, mood, 
   name, age, mbti, personality, job, 
   time, place, mood, creative_mode
 ):
-	prompt, neg_prompt = ch_img_maker.generate_prompts_from_keywords([mbti, personality, time, place, mood], job, age, name)
+	prompt, neg_prompt = ch_img_maker.generate_prompts_from_keywords([
+            mbti, personality, time, place, mood], job, age, name
+        )
 	print(f"Image Prompt: {prompt}")
 	print(f"Negative Prompt: {neg_prompt}")
 	img_filename = ch_img_maker.text2image(prompt, neg_prompt=neg_prompt)
