1c9c47365755fddc121f242326e66b5ae9a954bb
Sangjoon Han (Coding-pot) <jphan32@me.com>
2023-09-21 10:01:05 +0900

    radomly generate ID of the video file

--- interfaces/story_gen_ui.py
+++ interfaces/story_gen_ui.py
@@ -122,10 +122,10 @@ def video_gen(image, audio, title):
     client = Client(video_gen_client_url)
     
     result = client.predict(
-        "",
+        title,
         audio,
         image,
-        f"dummy.mp4",
+        f"{utils.id_generator()}.mp4",
         api_name="/predict"
     )
 
