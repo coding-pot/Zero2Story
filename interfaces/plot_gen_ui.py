af737ac00e29af0f3df250d1f4caff25fa898cd2
Chansung Park <deep.diver.csp@gmail.com>
2023-09-24 23:46:54 +0900

    organize prompt based on side char enables

--- interfaces/plot_gen_ui.py
+++ interfaces/plot_gen_ui.py
@@ -1,13 +1,33 @@
 from interfaces import utils
 from modules import palmchat
 
+def _add_side_character(
+    enable, prompt, cur_side_chars,
+    name, age, mbti, personality, job
+):
+    if enable:
+        prompt = prompt + f"""
+side character{cur_side_chars}: {{
+name: {name},
+job: {job},
+age: {age},
+mbti: {mbti},
+personality: {personality}
+}}"""
+        
+    return prompt, cur_side_chars
+    
+
 async def plot_gen(
     time, place, mood,
+    side_char_enable1, side_char_enable2, side_char_enable3,
     name1, age1, mbti1, personality1, job1,
     name2, age2, mbti2, personality2, job2,
     name3, age3, mbti3, personality3, job3,
     name4, age4, mbti4, personality4, job4,
 ):
+    cur_side_chars = 1
+    
     prompt = f"""You are a world-renowned novelist and TRPG creator. You specialize in long, descriptive sentences and enigmatic plots. As you write, you need to follow Ronald Tobias's plot theory. You also need to create a outline for your novel based on the input we give you, and generate a title based on the outline. You must create the outline at least more tham 2000 words long. YOU MUST FOLLOW THESE RULES.
 
 Output template is as follows: ```json{{"title": "title", "outline": "outline"}}```. DO NOT output anything other than JSON values. ONLY JSON is allowed.
@@ -23,31 +43,22 @@ age: {age1},
 mbti: {mbti1},
 personality: {personality1} 
 }}
-
-side character1: {{
-name: {name2},
-job: {job2},
-age: {age2},
-mbti: {mbti2},
-personality: {personality2} 
-}}
-
-side character2: {{
-name: {name3},
-job: {job3},
-age: {age3},
-mbti: {mbti3},
-personality: {personality3} 
-}}
-
-side character3: {{
-name: {name4},
-job: {job4},
-age: {age4},
-mbti: {mbti4},
-personality: {personality4} 
-}}
 """
+
+    prompt, cur_side_chars = _add_side_character(
+        side_char_enable1, prompt, cur_side_chars,
+        name2, job2, age2, mbti2, personality2
+    )
+    prompt, cur_side_chars = _add_side_character(
+        side_char_enable2, prompt, cur_side_chars,
+        name3, job3, age3, mbti3, personality3
+    )
+    prompt, cur_side_chars = _add_side_character(
+        side_char_enable3, prompt, cur_side_chars,
+        name4, job4, age4, mbti4, personality4
+    )
+    
+    print(f"generated prompt:\n{prompt}")
 
     response_json = None
     while response_json is None:
