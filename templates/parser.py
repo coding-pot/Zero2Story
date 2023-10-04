f1e9c31a1405c44b86450ba5811f8c61f56de93b
chansung <deep.diver.csp@gmail.com>
2023-10-04 15:04:37 +0900

    add comments about the keys in kwargs

--- templates/parser.py
+++ templates/parser.py
@@ -1,6 +1,12 @@
 import jinja2
 
 def gen_from_file(filename, kwargs):
+    # for basic template there are two keys that should be provided
+    # characters (list) 
+    # - each item has 'img' and 'name' keys
+    #
+    # items (stories)
+    # - each item has 'video', 'img', 'audio', and 'story'
     html_template = open(filename, "r")
 
     environment = jinja2.Environment()
