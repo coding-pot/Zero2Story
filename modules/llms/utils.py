a2db062e0f780469d333ff9a28116edf0c552e7b
june lee <melodysdreamj@gmail.com>
2023-11-17 17:18:52 +0900

    `

--- modules/llms/utils.py
+++ modules/llms/utils.py
@@ -8,5 +8,7 @@ def get_llm_factory(llm_type: str, api_key: str=None) -> 'PaLM':
         factory = ChatGPTFactory(openai_api_key=api_key)
     elif llm_type.lower() == 'llama2':
         factory = LLaMAFactory(hf_llama_api_key=api_key)
+
+    print("change llm to ",factory)
     
     return factory
